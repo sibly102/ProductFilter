ff6b8607c7f43bdf7180134d10ae25fe56753626
sibly102 <86411230+sibly102@users.noreply.github.com>
2024-12-24 23:07:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -115,6 +115,25 @@ function filterProduct(value){
    });
 }
 
+//search button click
+document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+    //loop through all elements
+    elements.forEach((element, index) => {
+        //check if text includes the search value
+
+        if(element.innerText.includes(searchInput.toUpperCase())){
+            //display matching card
+            cards[index].classList.remove("hide");
+        }
+        else{
+            cards[index].classList.add("hide");
+        }
+    })
+})
 
 window.onload = () => {
     filterProduct("all")
